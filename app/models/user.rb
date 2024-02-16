88cf219dd49e999b36b81427a560a6e8c1da6b08
thananjaya <thananjaya091993@gmail.com>
2024-02-16 21:54:12 +0530

    validations added for updating the kind

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   has_many :enrollments
   has_many :teachers, through: :enrollments
 
-  enum kind: {student: 0, teacher: 1, student_and_teacher: 2}
+  enum kind: { student: 0, teacher: 1, student_and_teacher: 2 }
 
-  # before_update :validate_user_kind
+  before_update :validate_user_kind
 
   def favorite_teachers
-    Enrollment.joins:user).where(users: {id: id}, favorite: true)
+    enrollments.where(favorite: true)
   end
 
   def self.classmates(user)
@@ -15,6 +15,12 @@ class User < ApplicationRecord
   end
 
   def validate_user_kind
-    
+    if Enrollment.find_by(teacher_id: id) && kind == 'student'
+      errors.add(:base, 'Kind can not be student because is teaching in at least one program')
+      raise ActiveRecord::Rollback
+    elsif Enrollment.find_by(user_id: id) && kind == 'teacher'
+      errors.add(:base, 'Kind can not be teacher because is studying in at least one program')
+      raise ActiveRecord::Rollback
+    end
   end
 end
