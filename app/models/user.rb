50df0fee1ea60b61189b18459bfeb2901c0860db
thananjaya <thananjaya091993@gmail.com>
2024-02-16 20:57:19 +0530

    mapped required associations and added model objects

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+  has_many :enrollments
+  has_many :teachers, through: :enrollments
+
+  enum kind: {student: 0, teacher: 1, student_and_teacher: 2}
+
+  # before_update :validate_user_kind
+
+  def favorite_teachers
+    Enrollment.joins:user).where(users: {id: id}, favorite: true)
+  end
+
+  def self.classmates(user)
+    User.joins(:enrollments).where.not(enrollments: {user_id: user.id}).where(enrollments: {program_id: user.enrollments.pluck(:program_id)})
+  end
+
+  def validate_user_kind
+    
+  end
 end
